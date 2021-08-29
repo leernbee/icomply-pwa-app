9a11ca99d2aeea1fc618c974fce069082687a3a4
segeok89 <lgdelrosario@outlook.com>
2021-08-29 10:18:50 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -55,12 +55,12 @@ function MyApp({ Component, pageProps }) {
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
         <meta name="description" content="Description" />
-        <title>Copperbell</title>
+        <title>A Powerful Regulatory Management Suite - Copperbell</title>
         <link rel="manifest" href="/manifest.json" />
         <link href="/icons/icon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
         <link href="/icons/icon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/icons/apple-touch-icon-180x180.png"></link>
-        <meta name="theme-color" content="#333333" />
+        <meta name="theme-color" content="#7C6A54" />
         <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async></script>
       </Head>
       <Component {...pageProps} />
